65eff6afcf6d860534e9a6a719795182e4873ae0
Ben Hooper <ben@labrys.io>
2023-11-15 20:56:18 +1000

    feat: added nav to ramblings page

--- next-app/pages/ramblings.tsx
+++ next-app/pages/ramblings.tsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 
 const Ramblings = () => (
   <Layout title="ramblings | hooper.link">
-    <div className="flex flex-col text-black dark:text-darkCream">
-      ramblings
-    </div>
+    <div className="flex flex-col">
+      <div className="flex flex-col text-black dark:text-darkCream">
+        ramblings
+      </div>
 
-    <Link href="/ramblings/tester">
-      <div className="flex bg-lightCream dark:bg-lightBlack">Rambling 1</div>
-    </Link>
+      <div className="flex flex-col">
+        <Link href="/ramblings/tester">
+          <div className="flex bg-lightCream dark:bg-lightBlack">
+            Rambling 1 (testing nav)
+          </div>
+        </Link>
+      </div>
+    </div>
   </Layout>
 );
 
