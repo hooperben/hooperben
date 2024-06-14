3a4404b6a30091456200bd3c3b8f0e41754db2cf
Ben Hooper <ben@labrys.io>
2024-06-14 22:26:05 +0200

    feat: starting a new article

---
+++ next-app/pages/ramblings/different-provers.tsx
@@ -0,0 +1,53 @@
+import "katex/dist/katex.min.css";
+import Head from "next/head";
+import Markdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import Layout from "../../components/Layout";
+
+const markdown = `placeholder`;
+
+const IndividualRamble = () => {
+  return (
+    <Layout title={`Different Proving Systems | hooper.link`}>
+      <Head>
+        <title>Different Proving Systems</title>
+
+        {/* Open Graph / Facebook Meta Tags */}
+        <meta
+          property="og:url"
+          content="https://hooper.link/ramblings/different-provers"
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="The 96er" />
+        <meta
+          property="og:description"
+          content="We raced 96 mario cart races"
+        />
+        {/* <meta property="og:image" content="https://hooper.link/the-96er.png" /> */}
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:domain" content="mysite.com" />
+        <meta
+          name="twitter:url"
+          content="https://hooper.link/ramblings/different-provers"
+        />
+        <meta name="twitter:title" content="The 96er" />
+        <meta
+          name="twitter:description"
+          content="We raced 96 mario cart races"
+        />
+        {/* <meta name="twitter:image" content="https://hooper.link/the-96er.png" /> */}
+      </Head>
+
+      <div className="flex flex-col w-full">
+        <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+          {markdown}
+        </Markdown>
+      </div>
+    </Layout>
+  );
+};
+
+export default IndividualRamble;
