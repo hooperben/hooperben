27d57348a0e272a840f528d5da39ce044b20566c
Ben Hooper <ben@labrys.io>
2024-08-11 14:44:41 +1000

    feat: added winner scripts

---
+++ scripts/check-winner.ts
@@ -0,0 +1,492 @@
+const results = [
+  {
+    track: "Mario Kart Stadium",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Water Park",
+    winner: "Gabba",
+    insights: "So many narnies",
+  },
+  {
+    track: "Sweet Sweet Canyon",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Thomp Ruins",
+    winner: "Ben",
+    insights: "",
+  },
+  {
+    track: "Mario Circuit",
+    winner: "Sweeney",
+    insights: "First consistent",
+  },
+  {
+    track: "Toad Harbour",
+    winner: "Noah",
+    insights: "",
+  },
+  {
+    track: "Twisted Mansion",
+    winner: "Ben",
+    insights: "2 twisted",
+  },
+  {
+    track: "Shy Guy Falls",
+    winner: "Gabba",
+    insights: "First hundy",
+  },
+  {
+    track: "Sunshine Airport",
+    winner: "Sweeney",
+    insights: "First podium",
+  },
+  {
+    track: "Dolphin Shoals",
+    winner: "Gabba",
+    insights: "Ben 10th and Sweeney 11th",
+  },
+  {
+    track: "Electrodome",
+    winner: "Sweeney",
+    insights: "First outside - 'I live outside'",
+  },
+  {
+    track: "Mount Wario",
+    winner: "Gabba",
+    insights: "Sweeney pool time",
+  },
+  {
+    track: "Cloudtop Cruise",
+    winner: "Sweeney",
+    insights: "KC",
+  },
+  {
+    track: "Bone Dry dunes",
+    winner: "Gabba",
+    insights: "Podium",
+  },
+  {
+    track: "Bowsers Castle",
+    winner: "Gabba",
+    insights: "First 200ie",
+  },
+  {
+    track: "Rainbow Road",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Moo Moo Meadows",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Mario Circuit",
+    winner: "Gabba",
+    insights: "Stupid narnie on the sean",
+  },
+  {
+    track: "Cheep Cheep Beach",
+    winner: "Sweeney",
+    insights: "Best map in the game",
+  },
+  {
+    track: "Toads Turnpike",
+    winner: "Gabba",
+    insights: "2 200s - podium",
+  },
+  {
+    track: "Dry Dry desert",
+    winner: "Link (First Mob)",
+    insights: "",
+  },
+  {
+    track: "Donut Planes",
+    winner: "Gabba",
+    insights: "Mob",
+  },
+  {
+    track: "Royal Raceway",
+    winner: "Gabba",
+    insights: "The mob is awake",
+  },
+  {
+    track: "DK Jungle",
+    winner: "Sweeney",
+    insights: "Podium",
+  },
+  {
+    track: "Wario Stadium",
+    winner: "Gabba",
+    insights: "Podium",
+  },
+  {
+    track: "Sherbet Land",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Melody Motorway",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Yoshi Valley",
+    winner: "Ben",
+    insights: "* as didn't hit the cannon on the first lap",
+  },
+  {
+    track: "Tick Tock Clock",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Pirahna Plant Pipeway",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Grumble Volcano",
+    winner: "Sweeney",
+    insights: "Link is on one",
+  },
+  {
+    track: "Rainbow Road",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Yoshi Circuit",
+    winner: "Sweeney",
+    insights: "First narnie snipe by Gabba",
+  },
+  {
+    track: "Excitebike arena",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Dragon Driftway",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Mute City",
+    winner: "Gabba",
+    insights: "Link Widening the gap",
+  },
+  {
+    track: "Warios Gold Mine",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Rainbow Road",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Ice Ice Outpost",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Hyrule Circuit",
+    winner: "Link (Still First Mob)",
+    insights: "All 4 players hit with single bomb",
+  },
+  {
+    track: "Baby Park",
+    winner: "Noah",
+    insights: "",
+  },
+  {
+    track: "Cheese Land",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Wild Woods",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Animal Crossing (Summer)",
+    winner: "Gabba",
+    insights: "summer days",
+  },
+  {
+    track: "Koopa City",
+    winner: "Gabba",
+    insights: "shit map",
+  },
+  {
+    track: "Ribbon Road",
+    winner: "Gabba",
+    insights: "this map is inside",
+  },
+  {
+    track: "Super bell Subway",
+    winner: "Gabba",
+    insights: "Link with the steal from Noah",
+  },
+  {
+    track: "Big Blue",
+    winner: "Ben",
+    insights: "Last Race wins (first half)",
+  },
+  {
+    track: "Paris Promenade",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Toad Circuit",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Choco Mountain",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Coconut Mall",
+    winner: "Sweeney",
+    insights: "Fendin off petey",
+  },
+  {
+    track: "Tokyo Blur",
+    winner: "Sweeney",
+    insights: "Noah Donut",
+  },
+  {
+    track: "Shroom Ridge",
+    winner: "Gabba",
+    insights: "Car got in the way of the sean",
+  },
+  {
+    track: "Sky Garden",
+    winner: "Tanooki Mario (Mob)",
+    insights: "First mob win of second half",
+  },
+  {
+    track: "Ninja Hideaway",
+    winner: "Gabba",
+    insights: "can a blue be sent backwards?",
+  },
+  {
+    track: "New York Minute",
+    winner: "Petey Piranha (Mob)",
+    insights: "sweeney though he had it",
+  },
+  {
+    track: "Mario Circuit 3",
+    winner: "Gabba",
+    insights: "Podium",
+  },
+  {
+    track: "Kalamari Desert",
+    winner: "Gabba",
+    insights: "Sweeney 11th",
+  },
+  {
+    track: "Waluigi Pinball",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Sydney Sprint",
+    winner: "Tanooki Mario (Mob)",
+    insights: "",
+  },
+  {
+    track: "Snowland",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Mushroom Gorge",
+    winner: "Petey Piranha (Mob)",
+    insights: "",
+  },
+  {
+    track: "Sky-High Sundae",
+    winner: "Gabba",
+    insights: "Ben hit my the mob",
+  },
+  {
+    track: "London Loop",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Boo Lake",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Alpine Pass",
+    winner: "Sweeney",
+    insights: "Petey still hitting",
+  },
+  {
+    track: "Maple Treeway",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Berlin Byways",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Peach Gardens",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Merry Mountain",
+    winner: "Sweeney",
+    insights: "The mob in force",
+  },
+  {
+    track: "Rainbow Road",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Amsterdam Drift",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Riverside Park",
+    winner: "Ben",
+    insights: "Great map",
+  },
+  {
+    track: "DK's Snowboard Cross",
+    winner: "Gabba",
+    insights: "touche",
+  },
+  {
+    track: "Yoshi's Island",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Bangkok Rush",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Mario Circuit",
+    winner: "Sweeney",
+    insights: "not many lightenings on the seans",
+  },
+  {
+    track: "Waluigi Stadium",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Singapore Speedway",
+    winner: "Sweeney",
+    insights: "Bowser Junior with the 2nd swoop",
+  },
+  {
+    track: "Athens Dash",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Daisy Cruiser",
+    winner: "Gabba",
+    insights: "Sweeney hit",
+  },
+  {
+    track: "Moonview Highway",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Squeaky Clean Sprint",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Los Angeles Laps",
+    winner: "Gabba",
+    insights: "he hate that",
+  },
+  {
+    track: "Sunset Wilds",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Koopa Cape",
+    winner: "Gabba",
+    insights: "sweeney unfortunate red",
+  },
+  {
+    track: "Vancouver Velocity",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Rome Avanti",
+    winner: "Ben",
+    insights: "",
+  },
+  {
+    track: "DK Mountain",
+    winner: "Sweeney",
+    insights: "",
+  },
+  {
+    track: "Daisy Circuit",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Piranha Plant Cove",
+    winner: "Gabba",
+    insights: "",
+  },
+  {
+    track: "Madrid Drive",
+    winner: "Ben",
+    insights: "",
+  },
+  {
+    track: "Rosalina's Ice World",
+    winner: "Gabba",
+    insights: "Podium",
+  },
+  {
+    track: "Bowser Castle 3",
+    winner: "Ben",
+    insights: "and thats how it can happen",
+  },
+  {
+    track: "Rainbow Road",
+    winner: "Sweeney",
+    insights: "Last race wins",
+  },
+];
+
+const gabeWon = results.filter((result) => result.winner === "Gabba");
+const sweeneyWon = results.filter((result) => result.winner === "Sweeney");
+const benWon = results.filter((result) => result.winner === "Ben");
+const noahWon = results.filter((result) => result.winner === "Noah");
+
+console.log("Gabba", gabeWon.length);
+console.log("Sweeney", sweeneyWon.length);
+console.log("Noah", noahWon.length);
+console.log("Ben", benWon.length);
